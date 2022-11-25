8e8b8b1c96c57d761a85ffd361a3dfcb3a73dd2e
Franky <Frankyzhou03@gmail.com>
2022-11-24 16:03:18 -0800

    test
    added comments

---
+++ Ukebox/BoxCode.ino
@@ -0,0 +1,18 @@
+
+/*
+Purpose of Code: to interface between 2 microcontrollers. One that will control the String Plucker,
+and the string presser. The pressers will be arranged in a 4x4 grid. 
+
+*/
+
+
+void setup(){
+
+}
+
+void loop(){
+
+}
+
+
+
